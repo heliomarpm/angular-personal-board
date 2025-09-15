161414fd44f8a7ee7e0b3fa6b3dba9affc45e5dc
Heliomar P. Marques <heliomarpm@gmail.com>
2025-09-15 20:06:21 -0300

    feat: add 404 page not found component with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '#', pathMatch: 'full' },
+  { path: '404', loadComponent: () => import('./views/page-not-found/page-not-found').then((c) => c.PageNotFound) },
+  { path: '**', redirectTo: '/404' },
+];
