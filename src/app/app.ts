1c3ed1f06f8ae9bd3d3f3fb53f85a9676b411e6c
Heliomar P. Marques <heliomarpm@gmail.com>
2025-09-15 19:25:26 -0300

    feat: add layout and styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('personal-board');
+export class App implements OnInit {
+  protected readonly dateTime = signal(new Date());
+
+  ngOnInit() {
+    setInterval(() => {
+      this.dateTime.set(new Date());
+    }, 1000);
+  }
 }
